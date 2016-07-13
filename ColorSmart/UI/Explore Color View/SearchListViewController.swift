a461a7de7a7bc8f3031004dfa588a71fe1dc0406
David <david@Alekss-MacBook-Pro.local>
2016-07-14 11:41:16 +1200

    Added header for search list.

--- ColorSmart/UI/Explore Color View/SearchListViewController.swift
+++ ColorSmart/UI/Explore Color View/SearchListViewController.swift
@@ -12,15 +12,11 @@ class SearchListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
         tableView.separatorStyle = .None
+        tableView.sectionHeaderHeight = 20
         tableView.registerNib(UINib(nibName: "SearchListViewCell", bundle:nil), forCellReuseIdentifier: "SearchListViewCell")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +30,17 @@ class SearchListViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let lbl = UILabel.init(frame: CGRectZero)
+        lbl.font = UIFont(name: "OpenSans-Regular", size: 14)
+        lbl.textColor = UIColor.rgb(101, green: 109, blue: 120)
+        lbl.text = "  SEARCH RESULTS"
+        
+        return lbl
+    }
+    
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -69,49 +76,5 @@ class SearchListViewController: UITableViewController {
         self.presentViewController(navCtrl, animated: true, completion: nil)
 
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
